fe6c0c10070031b0dc335c29acfb7fc82bbab046
Scooter <elscooter666@gmail.com>
2024-10-21 19:17:35 +0300

    Сборка + деплой на github pages

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmmlWebpackPlugin = require("html-webpack-plugin");
+const { template } = require("@babel/core");
+
+module.exports = {
+	mode: process.end.NODE_ENV || 'production',
+	entry: "./src/script.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.[contenthash].js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: "babel-loader",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.svg$/,
+				type: "asset/resource",
+			},
+		],
+	},
+	plugins: [
+		new HtmmlWebpackPlugin({
+			template: "./src/index.html",
+		}),
+	],
+	devServer: {
+		open: true,
+	},
+};
